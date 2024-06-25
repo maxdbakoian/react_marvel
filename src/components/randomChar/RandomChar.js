89da701b1c4cd4a5885817c0170f1e3eed4e3955
Max Bakoian <maxdbakoian@gmail.com>
2024-06-25 10:49:09 +0200

    code optimisation

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -11,32 +11,24 @@ class RandomChar extends Component {
     }
 
     state = {
-        name: null,
-        description: null,
-        thumbnail: null,
-        homepage: null,
-        wiki: null
+        char: {}
     }
 
     marvelService = new MarvelService();
+
+    onCharLoaded = (char) => {
+        this.setState({char})
+    }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1009225 - 1011196) + 1011196);
         this.marvelService
             .getCharacter(id)
-            .then(res => {
-                this.setState({
-                    name: res.data.results[0].name,
-                    description: res.data.results[0].description,
-                    thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-                    homepage: res.data.results[0].urls[0].url,
-                    wiki: res.data.results[0].urls[1].url
-                })
-            })
+            .then(this.onCharLoaded);
     }
 
     render() {
-        const {name, description, thumbnail, homepage, wiki} = this.state;
+        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
 
         return (
             <div className="randomchar">
